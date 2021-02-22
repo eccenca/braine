236820f690c44344dd0184ba86eb0664dfff508d
Edgard Marx <edgardmarx@Edgards-MacBook-Pro.local>
2021-02-22 15:53:50 +0100

    supporting k8s metrics through kafka consumer

--- relay/itops-relay.py
+++ relay/itops-relay.py
@@ -1,5 +1,5 @@
 # Sytem imports
-import yaml, time, threading
+import yaml, time, threading, json
 # CMEM imports
 from os import environ
 from cmem.cmempy.workspace.projects.project import get_projects
@@ -9,6 +9,7 @@ from cmem.cmempy.queries import SparqlQuery
 # Kubernetes imports
 from kubernetes import client, config, watch, client
 from kubernetes.client.rest import ApiException
+from confluent_kafka import Consumer, KafkaError
 
 # setup the environment for the connection to Corporate Memory
 environ["CMEM_BASE_URI"] = "https://braine.eccenca.dev/" # target cmem instance
@@ -33,6 +34,9 @@ podTemplateFile = "pod.tempalte.nt" # default pod template
 namespace = None # default namespace
 kubernetes_host = None
 kubernetes_api_key = None
+kafka_host = "localhost:9092"
+kafka_topic = 'k8s.metrics.nodes'
+origin = "kubernetes"
 
 mode = "sync" # default output streaming mode
 
@@ -65,10 +69,16 @@ if('interval' in transceiverConfig):
     interval = transceiverConfig['interval']
 if('mode' in transceiverConfig):
     mode = transceiverConfig['mode']
+if('origin' in transceiverConfig):
+    origin = transceiverConfig['origin']
 if('kubernetes.host' in transceiverConfig):
     kubernetes_host = transceiverConfig['kubernetes.host']
 if('kubernetes.key' in transceiverConfig):
     kubernetes_api_key = transceiverConfig['kubernetes.key']
+if('kafka.host' in transceiverConfig):
+    kafka_host = transceiverConfig['kafka.host']
+if('kafka.topic' in transceiverConfig):
+    kafka_topic = transceiverConfig['kafka.topic']
 
 # Setting CMEM config
 if('cmem.node.template' in transceiverConfig):
@@ -80,6 +90,14 @@ if('cmem.update.template' in transceiverConfig):
 if('cmem.update.graph' in transceiverConfig):
     update_sparql_graph = transceiverConfig['cmem.update.graph']
 
+kafka_settings = {
+    'bootstrap.servers': kafka_host,
+    'group.id': 'mygroup',
+    'client.id': 'client-1',
+    'enable.auto.commit': True,
+    'session.timeout.ms': 6000,
+    'default.topic.config': {'auto.offset.reset': 'smallest'}
+}
 
 def out(kind, name, namespace, timestamp, window, cpu, memory):  
     print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (kind, name, namespace, timestamp, window, cpu, memory))
@@ -97,7 +115,7 @@ def cmem(kind, name, namespace, timestamp, window, cpu, memory):
         # print(sparql_query)
         SparqlQuery(sparql_query).get_results()
     except Exception as e:
-        print("Exception when broadcasting to CMEM" + ": %s\n" % e)
+        print("Exception when broadcasting to CMEM" + ": %s" % e)
 
 def node2Triple(name, timestamp, window, cpu, memory):
     arguments = locals()
@@ -117,7 +135,7 @@ def pod2Triple(name, namespace, timestamp, window, cpu, memory):
         triples = fillTemplate(arguments, triples)
         return triples
     except Exception as e:
-        print("Exception when triplifying node" + name + ": %s\n" % e)
+        print("Exception when triplifying node" + name + ": %s" % e)
 
 def fillTemplate(arguments, template):
     for arg in arguments:
@@ -125,6 +143,39 @@ def fillTemplate(arguments, template):
             if(value is not None):
                 template = template.replace("{" + arg + "}", value)
     return template
+
+def streamK8sNodeMetrics(nodeMetrics, channels, channelOptions):
+    for i in nodeMetrics['items']:
+        iname = None
+        inamespace = None
+        itimestamp = None
+        iwindow = None
+        icpu = None
+        imemory = None
+        if(kind == "pod"):
+            iname = i['containers'][0]['name']
+            inamespace = i['metadata']['namespace']
+            itimestamp = i['timestamp']
+            iwindow = i['window'] # Window in seconds
+            icpu = i['containers'][0]['usage']['cpu'] # CPU usage in millicores
+            imemory = i['containers'][0]['usage']['memory'] # Memory in kibibytes (1024 bytes)
+        else:
+            iname = i['metadata']['name']
+            itimestamp = i['timestamp']
+            iwindow = i['window'] # Window in seconds
+            icpu = i['usage']['cpu'] # CPU usage in millicores
+            imemory = i['usage']['memory'] # Memory in kibibytes (1024 bytes)
+        # removing datatype tags
+        imemory = imemory.replace("Ki", "") 
+        icpu = icpu.replace("n", "")
+        iwindow = iwindow.replace("s", "")
+        for channel in channels: # broadcast in different channels
+            if(mode == "async"):
+                thr = threading.Thread(target=channelOptions[channel], args=(kind, iname, inamespace, itimestamp, iwindow, icpu, imemory), kwargs={})
+                thr.start() # runs
+            else:
+                channelOptions[channel](kind, iname, inamespace, itimestamp, iwindow, icpu, imemory)
+            
 
 # Kubernetes configs
 # Configs can be set in Configuration class directly or using helper utility
@@ -138,50 +189,46 @@ else:
 
 # Output default options
 channelOptions = {'stdout': out, 'cmem': cmem}
-customAPIV1 = None
-if(customConfig is None):
-    customAPIV1 = client.CustomObjectsApi()
-else:
-    with client.ApiClient(customConfig) as api_client:
-        customAPIV1 = client.CustomObjectsApi(api_client)
-starttime = time.time()
-try:
-    while True:
-        ret = None
-        if(namespace is not None):
-            ret = customAPIV1.list_namespaced_custom_object(group, version, namespace, kind + 's') # Just pod metrics for the default namespace
-        else:
-            ret = customAPIV1.list_cluster_custom_object(group, version, kind + 's') # All Metrics
-        for i in ret['items']:
-            iname = None
-            inamespace = None
-            itimestamp = None
-            iwindow = None
-            icpu = None
-            imemory = None
-            if(kind == "pod"):
-                iname = i['containers'][0]['name']
-                inamespace = i['metadata']['namespace']
-                itimestamp = i['timestamp']
-                iwindow = i['window'] # Window in seconds
-                icpu = i['containers'][0]['usage']['cpu'] # CPU usage in millicores
-                imemory = i['containers'][0]['usage']['memory'] # Memory in kibibytes (1024 bytes)
+
+if(origin == "kubernetes"):
+    customAPIV1 = None
+    if(customConfig is None):
+        customAPIV1 = client.CustomObjectsApi()
+    else:
+        with client.ApiClient(customConfig) as api_client:
+            customAPIV1 = client.CustomObjectsApi(api_client)
+    starttime = time.time()
+    try:
+        while True:
+            ret = None
+            if(namespace is not None):
+                ret = customAPIV1.list_namespaced_custom_object(group, version, namespace, kind + 's') # Just pod metrics for the default namespace
             else:
-                iname = i['metadata']['name']
-                itimestamp = i['timestamp']
-                iwindow = i['window'] # Window in seconds
-                icpu = i['usage']['cpu'] # CPU usage in millicores
-                imemory = i['usage']['memory'] # Memory in kibibytes (1024 bytes)
-            # removing datatype tags
-            imemory = imemory.replace("Ki", "") 
-            icpu = icpu.replace("n", "")
-            iwindow = iwindow.replace("s", "")
-            for channel in channels: # Broadcast in different channels
-                if(mode == "async"):
-                    thr = threading.Thread(target=channelOptions[channel], args=(kind, iname, inamespace, itimestamp, iwindow, icpu, imemory), kwargs={})
-                    thr.start() # runs
+                ret = customAPIV1.list_cluster_custom_object(group, version, kind + 's') # All Metrics
+            streamK8sNodeMetrics(ret, channels, channelOptions)
+            time.sleep(interval - ((time.time() - starttime) % interval))
+    except ApiException as e:
+            print("Exception when retrieving metrics from 'group' " + transceiverConfig['group'] + ": %s" % e)
+else:
+    c = Consumer(kafka_settings)
+    c.subscribe([kafka_topic])
+    try:
+        while True:
+            msg = c.poll(interval)
+            try:
+                if msg is None:
+                    continue
+                elif not msg.error():
+                    value = json.loads(msg.value())
+                    streamK8sNodeMetrics(value, channels, channelOptions)
+                elif msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('End of partition reached {0}/{1}'
+                        .format(msg.topic(), msg.partition()))
                 else:
-                    channelOptions[channel](kind, iname, inamespace, itimestamp, iwindow, icpu, imemory)
-        time.sleep(interval - ((time.time() - starttime) % interval))
-except ApiException as e:
-        print("Exception when retrieving metrics from 'group' " + transceiverConfig['group'] + ": %s\n" % e)
+                    print('Error occured: {0}'.format(msg.error().str()))
+            except Exception as e:
+                print("Exception when decoding metrics: %s" % e)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        c.close()
